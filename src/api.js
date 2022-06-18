bc12e42d4175181d72f3e24a7be158abb46e4887
billtrancon12 <69377326+billtrancon12@users.noreply.github.com>
2022-06-17 21:18:05 -0500

    debug

--- src/api.js
+++ src/api.js
@@ -24,10 +24,9 @@ async function getData(state){
     }
 }
 
-app.use(cors());
 app.use(express.json({limit: '50mb'}));
 
-router.use(function (req, res, next) {
+app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
