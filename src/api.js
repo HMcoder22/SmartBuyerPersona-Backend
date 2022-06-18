e5af00e14c44fd5ec9040bca9bb69b98edc8e06b
billtrancon12 <69377326+billtrancon12@users.noreply.github.com>
2022-06-17 21:16:00 -0500

    debug

--- src/api.js
+++ src/api.js
@@ -27,7 +27,7 @@ async function getData(state){
 app.use(cors());
 app.use(express.json({limit: '50mb'}));
 
-app.use(function (req, res, next) {
+router.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
