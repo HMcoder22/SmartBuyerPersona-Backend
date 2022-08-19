867d101e0cfa34f190c2801c6b55aebc397b713d
Vivek Madarapu <vivek.madarapu.800@k12.friscoisd.org>
2022-08-19 17:36:50 -0500

    Updated user details and error checking for admin api

--- src/admin.js
+++ src/admin.js
@@ -5,7 +5,7 @@ const app = express();
 const router = express.Router();
 const {updateData} = require('./database_tools.js');
 const {MongoClient} = require('mongodb');
-const {retrieveData} = require("./database_tools");
+const {retrieveData, deleteData} = require("./database_tools");
 
 require('dotenv').config();
 
@@ -14,16 +14,15 @@ app.use(express.json({limit: '50mb'}));
 
 const uri = `mongodb+srv://${process.env.db_username}:${process.env.db_password}@hagosmarketing.8mru08u.mongodb.net/?retryWrites=true&w=majority`
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });   // Create a client end-point
-console.log(uri);
 
 router.post("/manage/access", async function(req, res){
-    console.log(req.body);
-
     let validUser = true;
 
     try{
         await client.connect();
         const result = await retrieveData(client, "User", "Login", {username: req.body.email});  // Get a user info based on the username
+        if (result.body === "null")
+            validUser = false;
         await client.close();
     }
     catch(err){
@@ -32,12 +31,17 @@ router.post("/manage/access", async function(req, res){
     }
 
     if (!validUser){
-        res.json({success: false, error: 'invalid user'})
+        res.json(JSON.stringify({success: false, error: 'invalid user'}));
         return;
     }
 
     try{
         await client.connect();
+
+        //Very dangerous. Will put off for now.
+        // if(req.body.deleted){
+        //     await deleteData(client, "User", "Login", {username: req.body.email});
+        // }
 
         await updateData(client, "User", "Login", {username: req.body.email}, {$set: {
                 access: req.body.access}}, false)
@@ -45,18 +49,14 @@ router.post("/manage/access", async function(req, res){
 
         await client.close();
         res.json(JSON.stringify({success: true, error: ''}));
-        return;
     }
     catch(err){
         console.error(err);
         res.json(JSON.stringify({success: false, error: 'update failed'}));
-        return;
     }
 })
 
 async function getUsers() {
-
-
     try{
         await client.connect();
         const result = await client.db("User").collection("Login").find().toArray()
