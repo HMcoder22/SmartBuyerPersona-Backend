815d73ca34b6949efca2f2f8af09d8f23c91454b
Vivek Madarapu <vivek.madarapu.800@k12.friscoisd.org>
2022-09-15 18:39:09 -0500

    Removed openai queries from copy generator

--- src/copy_generator.js
+++ src/copy_generator.js
@@ -4,50 +4,51 @@ const cors = require('cors');
 const gibberish = require('gibberish-detective')();
 const app = express();
 const router = express.Router();
-const { Configuration, OpenAIApi } = require("openai");
+// const { Configuration, OpenAIApi } = require("openai");
 require('dotenv').config();
 
 gibberish.set("useCache", false);
-var copy_content = [];
 
 // Basic setting up for the backend
 app.use(express.json({limit: '100mb'}));
 app.use(cors());
 
 // Configuration for openai
-const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
-});
-const openai = new OpenAIApi(configuration);
+// const configuration = new Configuration({
+//     apiKey: process.env.OPENAI_API_KEY,
+// });
+// const openai = new OpenAIApi(configuration);
   
 async function getCopy(description, tone, type){  
-    const completion = await openai.createCompletion({
-        model: "text-davinci-002",
-        prompt: (type === 'product_checkbox') ? `Write a written copy content for social media for Facebook that has a ${tone} tone. \n\n Product: ${description}` : `Write a written copy content for social media for Facebook that has a ${tone} tone. \n\n Service: ${description}`,
-        max_tokens: 200,
-        temperature: 1,
-        top_p: 1,
-        frequency_penalty: 0.5,
-        presence_penalty: 0.7
-    });
-    return completion.data.choices[0].text;
+    // const completion = await openai.createCompletion({
+    //     model: "text-davinci-002",
+    //     prompt: (type === 'product_checkbox') ? `Write a written copy content for social media for Facebook that has a ${tone} tone. \n\n Product: ${description}` : `Write a written copy content for social media for Facebook that has a ${tone} tone. \n\n Service: ${description}`,
+    //     max_tokens: 200,
+    //     temperature: 1,
+    //     top_p: 1,
+    //     frequency_penalty: 0.5,
+    //     presence_penalty: 0.7
+    // });
+    // return completion.data.choices[0].text;
+    return (type === 'product_checkbox') ? `Write a written copy content for social media for Facebook that has a ${tone} tone. \n\n Product: ${description}` : `Write a written copy content for social media for Facebook that has a ${tone} tone. \n\n Service: ${description}`;
 }
 
 async function getHashtag(description, tone, type){  
-    const completion = await openai.createCompletion({
-        model: "text-davinci-002",
-        prompt: (type === 'product_checkbox') ? `Write only one completed hashtag for social media that has a ${tone} tone. \n\n Product: ${description}` : `Write a hashtag content for social media for Facebook that has a ${tone} tone. \n\n Service: ${description}`,
-        max_tokens: 50,
-        temperature: 1,
-        top_p: 1,
-        frequency_penalty: 0.5,
-        presence_penalty: 0.7
-    });
-    return completion.data.choices[0].text;
+    // const completion = await openai.createCompletion({
+    //     model: "text-davinci-002",
+    //     prompt: (type === 'product_checkbox') ? `Write only one completed hashtag for social media that has a ${tone} tone. \n\n Product: ${description}` : `Write a hashtag content for social media for Facebook that has a ${tone} tone. \n\n Service: ${description}`,
+    //     max_tokens: 50,
+    //     temperature: 1,
+    //     top_p: 1,
+    //     frequency_penalty: 0.5,
+    //     presence_penalty: 0.7
+    // });
+    // return completion.data.choices[0].text;
+    return (type === 'product_checkbox') ? `Write only one completed hashtag for social media that has a ${tone} tone. \n\n Product: ${description}` : `Write a hashtag content for social media for Facebook that has a ${tone} tone. \n\n Service: ${description}`;
 }
 
 
-app.post("/copy_generator/written", async function(req, res){
+router.post("/generator/written", async function(req, res){
     if(gibberish.detect(req.body.description)){
         res.json({
             statusCode: 200,
@@ -76,7 +77,7 @@ app.post("/copy_generator/written", async function(req, res){
     }
 })
 
-app.post("/copy_generator/hashtag", async function(req, res){
+router.post("/generator/hashtag", async function(req, res){
     if(gibberish.detect(req.body.description)){
         res.json({
             statusCode: 200,
@@ -106,5 +107,10 @@ app.post("/copy_generator/hashtag", async function(req, res){
     }
 })
 
-app.listen(4000);
+app.use(`/.netlify/functions/copy_generator`, router);
+
+module.exports = app;
+module.exports.handler = serverless(app);
+
+// app.listen(4000);
 
